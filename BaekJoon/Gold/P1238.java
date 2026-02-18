33de8ff76df1fbc93fdbdf24c16cfa2e221a19fb
parkgunwook <ss944161269441@gmail.com>
2026-02-18 11:23:41 +0900

    [feat]: solve 1238

---
+++ BaekJoon/Gold/P1238.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class P1238 {
+    public static class Node implements Comparable<Node> {
+        int index;
+        int weight;
+
+        Node(int index, int weight) {
+            this.index = index;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+
+    public static List<List<Node>> collection = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int vertexNumber = Integer.parseInt(st.nextToken());
+        int lineNumber = Integer.parseInt(st.nextToken());
+        int startVertex = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i <= vertexNumber; i++) {
+            collection.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < lineNumber; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            collection.get(from).add(new Node(to, weight));
+        }
+
+        int[] XtoHome = Dijkstra(startVertex, vertexNumber);
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i <= vertexNumber; i++) {
+            int[] HometoX = Dijkstra(i, vertexNumber);
+
+            if(HometoX[startVertex] + XtoHome[i] > max) {
+                max = HometoX[startVertex] + XtoHome[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    public static int[] Dijkstra(int startVertex, int vertexNumber) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(startVertex, 0));
+        boolean[] isVisited = new boolean[vertexNumber + 1];
+        int[] distance = new int[vertexNumber + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[startVertex] = 0;
+
+        while(!pq.isEmpty()) {
+            Node currentNode = pq.poll();
+            int currentVertex = currentNode.index;
+
+            if(isVisited[currentVertex]) {
+                continue;
+            }
+
+            isVisited[currentVertex] = true;
+
+            for(int i = 0; i < collection.get(currentVertex).size(); i++) {
+                Node nextNode = collection.get(currentVertex).get(i);
+
+                if(distance[nextNode.index] > distance[currentVertex] + nextNode.weight) {
+                    distance[nextNode.index] = distance[currentVertex] + nextNode.weight;
+                    pq.add(new Node(nextNode.index, distance[nextNode.index]));
+                }
+            }
+        }
+
+        return distance;
+    }
+}
